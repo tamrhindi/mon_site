8064373c1acabff5601deceaee32ad72e2fe7def
tamrhindi <hadid@mail.rugit config --global user.email hadid@mail.ru>
2018-11-16 14:39:31 +0100

    Premére version du projet

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Mon site</title>
+</head>
+<body>
+<div class="container">
+<div class="display-4 text-center">Inscription</div>
+
+   
+<form method="POST" action=""  class="shadow p-3 mb-5 rounded " style="background-color:#ececec;">
+  <div class="form-row">
+  <div class="col form-group col-sm-12">
+    <label for="Pseudo">Pseudo</label>
+      <input type="text" class="form-control" id="Pseudo" name="psedo"  placeholder="Pseudo">
+    </div> <!--pseudo-->
+       <div class="col  form-group col-sm-12">
+      <label for="inputEmail">Email</label>
+      <input type="text" class="form-control" id="inputEmail" name="email" placeholder="Email">
+    </div> <!--Email-->
+
+    <div class="col form-group col-sm-12">
+      <label for="inputPassword">Password</label>
+      <input type="password" class="form-control"  name="Password" id="inputPassword" placeholder="Password">
+    </div> <!--password-->
+    <div class="col form-group col-sm-12">
+      <label for="ConfirmPassword">Confirmer password</label>
+      <input type="password" class="form-control"  name="Confirm"id="ConfirmPassword" placeholder="Password">
+    </div> <!--Confirmer password-->
+    
+ 
+   
+      <button type="submit" class="col-sm-12 btn btn-info">Inscription</button>
+  </div>
+</form>
+
+<!---->
+
+</div>
+
+
+
+
+
+
+
+
+
+
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ 
+</body>
+</html>
